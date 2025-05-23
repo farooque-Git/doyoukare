d5ea81934e51d3fa6138f57d1255b5eb9c89b685
Farooque <farooque.reactjs@gmail.com>
2025-05-23 23:48:20 +0530

    fix al the issue in dropdwon and range

--- script.js
+++ script.js
@@ -1,513 +1,8 @@
-const stateData = {
-  Alabama: {
-    state: "Alabama",
-    license: "CNA/CMA",
-    concatenate: "AlabamaCNA/CMA",
-    payRate: 16,
-    withOvertime: 24,
-    overtimeCost: 6.96,
-    realHourlyCost: 30.96,
-    kareRate: 20.83,
-    savings: 10.13,
-  },
-  Colorado: {
-    state: "Colorado",
-    license: "CNA/CMA",
-    concatenate: "ColoradoCNA/CMA",
-    payRate: 21.32,
-    withOvertime: 31.98,
-    overtimeCost: 9.27,
-    realHourlyCost: 41.25,
-    kareRate: 31.66,
-    savings: 9.59,
-  },
-  Connecticut: {
-    state: "Connecticut",
-    license: "CNA/CMA",
-    concatenate: "ConnecticutCNA/CMA",
-    payRate: 24.51,
-    withOvertime: 36.77,
-    overtimeCost: 11.08,
-    realHourlyCost: 47.85,
-    kareRate: 32.54,
-    savings: 15.31,
-  },
-  Delaware: {
-    state: "Delaware",
-    license: "CNA/CMA",
-    concatenate: "DelawareCNA/CMA",
-    payRate: 19.03,
-    withOvertime: 28.54,
-    overtimeCost: 8.6,
-    realHourlyCost: 37.14,
-    kareRate: 24.56,
-    savings: 12.58,
-  },
-  Florida: {
-    state: "Florida",
-    license: "CNA/CMA",
-    concatenate: "FloridaCNA/CMA",
-    payRate: 15.76,
-    withOvertime: 23.65,
-    overtimeCost: 7.13,
-    realHourlyCost: 30.79,
-    kareRate: 19.32,
-    savings: 11.47,
-  },
-  Georgia: {
-    state: "Georgia",
-    license: "CNA/CMA",
-    concatenate: "GeorgiaCNA/CMA",
-    payRate: 15.55,
-    withOvertime: 23.32,
-    overtimeCost: 7.03,
-    realHourlyCost: 30.35,
-    kareRate: 19.27,
-    savings: 11.08,
-  },
-  Hawaii: {
-    state: "Hawaii",
-    license: "CNA/CMA",
-    concatenate: "HawaiiCNA/CMA",
-    payRate: 26.08,
-    withOvertime: 39.12,
-    overtimeCost: 11.79,
-    realHourlyCost: 50.91,
-    kareRate: 33.5,
-    savings: 17.41,
-  },
-  Idaho: {
-    state: "Idaho",
-    license: "CNA/CMA",
-    concatenate: "IdahoCNA/CMA",
-    payRate: 15.26,
-    withOvertime: 22.89,
-    overtimeCost: 6.9,
-    realHourlyCost: 29.79,
-    kareRate: 20.03,
-    savings: 9.76,
-  },
-  Illinois: {
-    state: "Illinois",
-    license: "CNA/CMA",
-    concatenate: "IllinoisCNA/CMA",
-    payRate: 17.64,
-    withOvertime: 26.46,
-    overtimeCost: 7.98,
-    realHourlyCost: 34.44,
-    kareRate: 22.66,
-    savings: 11.78,
-  },
-  Indiana: {
-    state: "Indiana",
-    license: "CNA/CMA",
-    concatenate: "IndianaCNA/CMA",
-    payRate: 16.72,
-    withOvertime: 25.08,
-    overtimeCost: 7.58,
-    realHourlyCost: 32.66,
-    kareRate: 21.62,
-    savings: 11.04,
-  },
-  Iowa: {
-    state: "Iowa",
-    license: "CNA/CMA",
-    concatenate: "IowaCNA/CMA",
-    payRate: 17.17,
-    withOvertime: 25.76,
-    overtimeCost: 7.79,
-    realHourlyCost: 33.55,
-    kareRate: 22.88,
-    savings: 10.67,
-  },
-  Kansas: {
-    state: "Kansas",
-    license: "CNA/CMA",
-    concatenate: "KansasCNA/CMA",
-    payRate: 15.71,
-    withOvertime: 23.57,
-    overtimeCost: 7.13,
-    realHourlyCost: 30.7,
-    kareRate: 20.28,
-    savings: 10.42,
-  },
-  Kentucky: {
-    state: "Kentucky",
-    license: "CNA/CMA",
-    concatenate: "KentuckyCNA/CMA",
-    payRate: 15.89,
-    withOvertime: 23.83,
-    overtimeCost: 7.2,
-    realHourlyCost: 30.85,
-    kareRate: 19.48,
-    savings: 11.37,
-  },
-  Louisiana: {
-    state: "Louisiana",
-    license: "CNA/CMA",
-    concatenate: "LouisianaCNA/CMA",
-    payRate: 15.27,
-    withOvertime: 22.9,
-    overtimeCost: 6.9,
-    realHourlyCost: 29.8,
-    kareRate: 19.43,
-    savings: 10.37,
-  },
-  Maine: {
-    state: "Maine",
-    license: "CNA/CMA",
-    concatenate: "MaineCNA/CMA",
-    payRate: 18.17,
-    withOvertime: 27.26,
-    overtimeCost: 8.23,
-    realHourlyCost: 35.49,
-    kareRate: 22.85,
-    savings: 12.64,
-  },
-  Maryland: {
-    state: "Maryland",
-    license: "CNA/CMA",
-    concatenate: "MarylandCNA/CMA",
-    payRate: 21.91,
-    withOvertime: 32.87,
-    overtimeCost: 9.93,
-    realHourlyCost: 42.8,
-    kareRate: 30.43,
-    savings: 12.37,
-  },
-  Massachusetts: {
-    state: "Massachusetts",
-    license: "CNA/CMA",
-    concatenate: "MassachusettsCNA/CMA",
-    payRate: 25.75,
-    withOvertime: 38.63,
-    overtimeCost: 11.68,
-    realHourlyCost: 50.31,
-    kareRate: 34.46,
-    savings: 15.85,
-  },
-  Michigan: {
-    state: "Michigan",
-    license: "CNA/CMA",
-    concatenate: "MichiganCNA/CMA",
-    payRate: 17.22,
-    withOvertime: 25.84,
-    overtimeCost: 7.82,
-    realHourlyCost: 33.65,
-    kareRate: 23.44,
-    savings: 10.21,
-  },
-  Minnesota: {
-    state: "Minnesota",
-    license: "CNA/CMA",
-    concatenate: "MinnesotaCNA/CMA",
-    payRate: 19.45,
-    withOvertime: 29.18,
-    overtimeCost: 8.88,
-    realHourlyCost: 38.06,
-    kareRate: 27.13,
-    savings: 10.93,
-  },
-  Mississippi: {
-    state: "Mississippi",
-    license: "CNA/CMA",
-    concatenate: "MississippiCNA/CMA",
-    payRate: 14.43,
-    withOvertime: 21.64,
-    overtimeCost: 6.53,
-    realHourlyCost: 28.17,
-    kareRate: 17.64,
-    savings: 10.53,
-  },
-  Missouri: {
-    state: "Missouri",
-    license: "CNA/CMA",
-    concatenate: "MissouriCNA/CMA",
-    payRate: 15.35,
-    withOvertime: 23.03,
-    overtimeCost: 6.97,
-    realHourlyCost: 30.0,
-    kareRate: 19.3,
-    savings: 10.7,
-  },
-  Montana: {
-    state: "Montana",
-    license: "CNA/CMA",
-    concatenate: "MontanaCNA/CMA",
-    payRate: 15.61,
-    withOvertime: 23.41,
-    overtimeCost: 7.07,
-    realHourlyCost: 30.1,
-    kareRate: 19.96,
-    savings: 10.14,
-  },
-  Nebraska: {
-    state: "Nebraska",
-    license: "CNA/CMA",
-    concatenate: "NebraskaCNA/CMA",
-    payRate: 15.95,
-    withOvertime: 23.92,
-    overtimeCost: 7.29,
-    realHourlyCost: 30.61,
-    kareRate: 19.82,
-    savings: 10.79,
-  },
-  Nevada: {
-    state: "Nevada",
-    license: "CNA/CMA",
-    concatenate: "NevadaCNA/CMA",
-    payRate: 19.88,
-    withOvertime: 29.82,
-    overtimeCost: 9.03,
-    realHourlyCost: 38.84,
-    kareRate: 26.85,
-    savings: 11.99,
-  },
-  NewHampshire: {
-    state: "New Hampshire",
-    license: "CNA/CMA",
-    concatenate: "New HampshireCNA/CMA",
-    payRate: 21.35,
-    withOvertime: 32.02,
-    overtimeCost: 9.36,
-    realHourlyCost: 41.39,
-    kareRate: 28.19,
-    savings: 13.2,
-  },
-  NewJersey: {
-    state: "New Jersey",
-    license: "CNA/CMA",
-    concatenate: "New JerseyCNA/CMA",
-    payRate: 25.68,
-    withOvertime: 38.52,
-    overtimeCost: 11.52,
-    realHourlyCost: 49.88,
-    kareRate: 33.77,
-    savings: 16.11,
-  },
-  NewMexico: {
-    state: "New Mexico",
-    license: "CNA/CMA",
-    concatenate: "New MexicoCNA/CMA",
-    payRate: 15.08,
-    withOvertime: 22.62,
-    overtimeCost: 6.73,
-    realHourlyCost: 29.35,
-    kareRate: 19.84,
-    savings: 9.51,
-  },
-  NewYork: {
-    state: "New York",
-    license: "CNA/CMA",
-    concatenate: "New YorkCNA/CMA",
-    payRate: 24.34,
-    withOvertime: 36.51,
-    overtimeCost: 10.96,
-    realHourlyCost: 47.47,
-    kareRate: 31.75,
-    savings: 15.72,
-  },
-  NorthCarolina: {
-    state: "North Carolina",
-    license: "CNA/CMA",
-    concatenate: "North CarolinaCNA/CMA",
-    payRate: 15.76,
-    withOvertime: 23.65,
-    overtimeCost: 7.13,
-    realHourlyCost: 30.79,
-    kareRate: 19.32,
-    savings: 11.47,
-  },
-  NorthDakota: {
-    state: "North Dakota",
-    license: "CNA/CMA",
-    concatenate: "North DakotaCNA/CMA",
-    payRate: 17.55,
-    withOvertime: 26.32,
-    overtimeCost: 7.94,
-    realHourlyCost: 34.26,
-    kareRate: 22.69,
-    savings: 11.57,
-  },
-  Ohio: {
-    state: "Ohio",
-    license: "CNA/CMA",
-    concatenate: "OhioCNA/CMA",
-    payRate: 16.56,
-    withOvertime: 24.83,
-    overtimeCost: 7.39,
-    realHourlyCost: 32.22,
-    kareRate: 21.03,
-    savings: 11.19,
-  },
-  Oklahoma: {
-    state: "Oklahoma",
-    license: "CNA/CMA",
-    concatenate: "OklahomaCNA/CMA",
-    payRate: 15.0,
-    withOvertime: 22.49,
-    overtimeCost: 6.68,
-    realHourlyCost: 28.68,
-    kareRate: 18.94,
-    savings: 9.74,
-  },
-  Oregon: {
-    state: "Oregon",
-    license: "CNA/CMA",
-    concatenate: "OregonCNA/CMA",
-    payRate: 23.1,
-    withOvertime: 34.65,
-    overtimeCost: 10.44,
-    realHourlyCost: 45.09,
-    kareRate: 30.53,
-    savings: 14.56,
-  },
-  Pennsylvania: {
-    state: "Pennsylvania",
-    license: "CNA/CMA",
-    concatenate: "PennsylvaniaCNA/CMA",
-    payRate: 17.82,
-    withOvertime: 26.73,
-    overtimeCost: 8.1,
-    realHourlyCost: 34.83,
-    kareRate: 23.02,
-    savings: 11.81,
-  },
-  RhodeIsland: {
-    state: "Rhode Island",
-    license: "CNA/CMA",
-    concatenate: "Rhode IslandCNA/CMA",
-    payRate: 22.89,
-    withOvertime: 34.34,
-    overtimeCost: 10.44,
-    realHourlyCost: 44.78,
-    kareRate: 28.43,
-    savings: 16.35,
-  },
-  SouthCarolina: {
-    state: "South Carolina",
-    license: "CNA/CMA",
-    concatenate: "South CarolinaCNA/CMA",
-    payRate: 15.17,
-    withOvertime: 22.76,
-    overtimeCost: 6.77,
-    realHourlyCost: 29.53,
-    kareRate: 19.16,
-    savings: 10.37,
-  },
-  SouthDakota: {
-    state: "South Dakota",
-    license: "CNA/CMA",
-    concatenate: "South DakotaCNA/CMA",
-    payRate: 16.89,
-    withOvertime: 25.33,
-    overtimeCost: 7.6,
-    realHourlyCost: 32.93,
-    kareRate: 22.55,
-    savings: 10.38,
-  },
-  Tennessee: {
-    state: "Tennessee",
-    license: "CNA/CMA",
-    concatenate: "TennesseeCNA/CMA",
-    payRate: 15.3,
-    withOvertime: 22.95,
-    overtimeCost: 6.84,
-    realHourlyCost: 29.79,
-    kareRate: 19.24,
-    savings: 10.55,
-  },
-  Texas: {
-    state: "Texas",
-    license: "CNA/CMA",
-    concatenate: "TexasCNA/CMA",
-    payRate: 15.64,
-    withOvertime: 23.46,
-    overtimeCost: 7.0,
-    realHourlyCost: 30.46,
-    kareRate: 19.43,
-    savings: 11.03,
-  },
-  Utah: {
-    state: "Utah",
-    license: "CNA/CMA",
-    concatenate: "UtahCNA/CMA",
-    payRate: 17.25,
-    withOvertime: 25.86,
-    overtimeCost: 7.91,
-    realHourlyCost: 33.77,
-    kareRate: 22.71,
-    savings: 11.06,
-  },
-  Vermont: {
-    state: "Vermont",
-    license: "CNA/CMA",
-    concatenate: "VermontCNA/CMA",
-    payRate: 19.22,
-    withOvertime: 28.83,
-    overtimeCost: 8.99,
-    realHourlyCost: 37.82,
-    kareRate: 25.8,
-    savings: 11.02,
-  },
-  Virginia: {
-    state: "Virginia",
-    license: "CNA/CMA",
-    concatenate: "VirginiaCNA/CMA",
-    payRate: 16.3,
-    withOvertime: 24.44,
-    overtimeCost: 7.31,
-    realHourlyCost: 31.75,
-    kareRate: 20.81,
-    savings: 10.94,
-  },
-  Washington: {
-    state: "Washington",
-    license: "CNA/CMA",
-    concatenate: "WashingtonCNA/CMA",
-    payRate: 23.57,
-    withOvertime: 35.36,
-    overtimeCost: 10.67,
-    realHourlyCost: 45.91,
-    kareRate: 30.81,
-    savings: 15.1,
-  },
-  WestVirginia: {
-    state: "West Virginia",
-    license: "CNA/CMA",
-    concatenate: "West VirginiaCNA/CMA",
-    payRate: 15.22,
-    withOvertime: 22.84,
-    overtimeCost: 6.81,
-    realHourlyCost: 29.66,
-    kareRate: 19.27,
-    savings: 10.39,
-  },
-  Wisconsin: {
-    state: "Wisconsin",
-    license: "CNA/CMA",
-    concatenate: "WisconsinCNA/CMA",
-    payRate: 17.41,
-    withOvertime: 26.12,
-    overtimeCost: 7.88,
-    realHourlyCost: 33.99,
-    kareRate: 23.18,
-    savings: 10.81,
-  },
-  Wyoming: {
-    state: "Wyoming",
-    license: "CNA/CMA",
-    concatenate: "WyomingCNA/CMA",
-    payRate: 23.52,
-    withOvertime: 35.28,
-    overtimeCost: 10.22,
-    realHourlyCost: 45.5,
-    kareRate: 34.49,
-    savings: 11.01,
-  },
-};
+// Import state data
+import { stateData } from "./data.js";
 
 const stateSelect = document.getElementById("state");
+const licenseSelect = document.getElementById("license");
 const payrateEl = document.querySelector(".summary p:nth-of-type(1) span");
 const withOvertimeEl = document.querySelector(".summary p:nth-of-type(2) span");
 const overtimeCostFixedEl = document.querySelector(
@@ -522,6 +17,10 @@ const kareRateEl = document.querySelector(
 const savingsEl = document.querySelector(
   ".summary p:nth-of-type(6) strong.orange"
 );
+
+// Update summary text based on license type
+const summaryTexts = document.querySelectorAll(".summary p");
+const licenseTypeText = summaryTexts[0].textContent.split(" ")[1]; // Get the license type text
 
 const hoursRange = document.getElementById("hours");
 const percentRange = document.getElementById("percentage");
@@ -538,6 +37,13 @@ const hourText = document.getElementById("hourText");
 let OVERTIME_COST_PER_HOUR = 41.25; // default
 let KARE_COST_PER_HOUR = 31.66; // default
 
+// License type multipliers (adjust these based on your actual rates)
+const licenseMultipliers = {
+  "CNA / CMA": 1,
+  "LPN/ LVN": 1.5,
+  RN: 2.2,
+};
+
 function formatCurrency(value) {
   return `$${value.toLocaleString(undefined, {
     minimumFractionDigits: 2,
@@ -545,18 +51,41 @@ function formatCurrency(value) {
   })}`;
 }
 
-function updateSummary(state) {
+function updateSummaryTexts(licenseType) {
+  summaryTexts.forEach((p) => {
+    const text = p.textContent;
+    if (text.includes("CNA / CMA")) {
+      p.textContent = text.replace("CNA / CMA", licenseType);
+    }
+  });
+}
+
+function updateSummary(state, licenseType) {
   const data = stateData[state];
   if (data) {
-    payrateEl.textContent = formatCurrency(data.payRate);
-    withOvertimeEl.textContent = formatCurrency(data.withOvertime);
-    overtimeCostFixedEl.textContent = formatCurrency(data.overtimeCost);
-    realHourlyCostEl.textContent = formatCurrency(data.realHourlyCost);
-    kareRateEl.textContent = formatCurrency(data.kareRate);
-    savingsEl.textContent = formatCurrency(data.savings);
+    const multiplier = licenseMultipliers[licenseType] || 1;
 
-    OVERTIME_COST_PER_HOUR = data.realHourlyCost;
-    KARE_COST_PER_HOUR = data.kareRate;
+    // Apply multiplier to all rates
+    const adjustedPayRate = data.payRate * multiplier;
+    const adjustedWithOvertime = data.withOvertime * multiplier;
+    const adjustedOvertimeCost = data.overtimeCost * multiplier;
+    const adjustedRealHourlyCost = data.realHourlyCost * multiplier;
+    const adjustedKareRate = data.kareRate * multiplier;
+    const adjustedSavings = data.savings * multiplier;
+
+    payrateEl.textContent = formatCurrency(adjustedPayRate);
+    withOvertimeEl.textContent = formatCurrency(adjustedWithOvertime);
+    overtimeCostFixedEl.textContent = formatCurrency(adjustedOvertimeCost);
+    realHourlyCostEl.textContent = formatCurrency(adjustedRealHourlyCost);
+    kareRateEl.textContent = formatCurrency(adjustedKareRate);
+    savingsEl.textContent = formatCurrency(adjustedSavings);
+
+    OVERTIME_COST_PER_HOUR = adjustedRealHourlyCost;
+    KARE_COST_PER_HOUR = adjustedKareRate;
+
+    // Update the summary texts to reflect the selected license type
+    updateSummaryTexts(licenseType);
+
     calculate();
   }
 }
@@ -592,18 +121,47 @@ function calculate() {
   hourText.textContent = `${hours}`;
 }
 
-[stateSelect, hoursRange, percentRange, hoursInput, percentInput].forEach(
-  (input) => {
-    input.addEventListener("input", (e) => {
-      if (e.target === stateSelect) {
-        updateSummary(stateSelect.value);
-      } else {
-        // For inputs other than state select, just recalc
-        calculate();
-      }
-    });
-  }
-);
+// Set up event listeners for each input
+stateSelect.addEventListener("change", () => {
+  updateSummary(stateSelect.value, licenseSelect.value);
+});
+
+licenseSelect.addEventListener("change", () => {
+  updateSummary(stateSelect.value, licenseSelect.value);
+});
+
+hoursRange.addEventListener("input", () => {
+  hoursInput.value = hoursRange.value;
+  calculate();
+});
+
+percentRange.addEventListener("input", () => {
+  percentInput.value = percentRange.value;
+  calculate();
+});
+
+hoursInput.addEventListener("input", () => {
+  let val = Math.min(
+    Math.max(parseInt(hoursInput.value) || 0, parseInt(hoursRange.min) || 0),
+    parseInt(hoursRange.max) || 500
+  );
+  hoursInput.value = val;
+  hoursRange.value = val;
+  calculate();
+});
+
+percentInput.addEventListener("input", () => {
+  let val = Math.min(
+    Math.max(
+      parseInt(percentInput.value) || 0,
+      parseInt(percentRange.min) || 0
+    ),
+    parseInt(percentRange.max) || 150
+  );
+  percentInput.value = val;
+  percentRange.value = val;
+  calculate();
+});
 
 // Initial load
-updateSummary(stateSelect.value);
+updateSummary(stateSelect.value, licenseSelect.value);
